2c80bfae588cb84736338c9d7c3e31b7c21cb3cf
Jorge Venegas <brangelina01@gmail.com>
2020-07-09 03:58:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
    letter_array = ["will","do","the","am"]
-   wow = letter_array.shift 
+   letter_array [am] 
 end
 
 def retrieve_first_element_from_array(array)
