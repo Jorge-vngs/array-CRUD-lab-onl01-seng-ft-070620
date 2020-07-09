6ebd85d5f56f5efa7fe65530cb4d8b6f7849e306
Jorge Venegas <brangelina01@gmail.com>
2020-07-09 03:46:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   letter_array = ["element!","variable!","house!"]
-  letter_array << "arrays!"
+  letter_array.unshift "arrays!"
 end
 
 def remove_element_from_end_of_array(array)
