aadfe36ce664489f256e4a766b3eee0e6d289b1c
Jorge Venegas <brangelina01@gmail.com>
2020-07-09 03:28:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   letter_array = [a,b,c]
-  letter_array << ""
+  letter_array << "d"
 end
 
 def add_element_to_start_of_array(array, element)
