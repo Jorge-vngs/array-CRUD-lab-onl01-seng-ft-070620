e0297c907d0f5fb2dc3bb0a430d76e3b1f0f7e70
Jorge Venegas <brangelina01@gmail.com>
2020-07-09 03:13:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  number_array.push ("5")
+  number_array.push ("5
 end
 
 def add_element_to_start_of_array(array, element)
