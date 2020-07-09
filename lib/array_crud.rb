562d137723611e1fba3e2425c155ea5dc083ee85
Jorge Venegas <brangelina01@gmail.com>
2020-07-09 03:51:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
    letter_array = ["element!","variable!","arrays!"]
-    = letter_array.pop
+   arrays = letter_array.pop
 end
 
 def remove_element_from_start_of_array(array)
